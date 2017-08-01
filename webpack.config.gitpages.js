b8adf87fe621be71e78bf296058870f1ed13f570
boeny <boeniareadmirr@gmail.com>
2017-08-01 15:04:59 +0300

    git-pages

--- webpack.config.gitpages.js
+++ webpack.config.gitpages.js
@@ -1,23 +1,10 @@
-const dest = __dirname+'/dist/js';
-const fs = require('fs');
-
-fs.readdir(dest, (err, files) => {
-	if (err) throw err;
-	
-	files = files.filter((file) => file.match(/.*\.map/ig));
-	
-	for (var i = 0; i < files.length; i++){
-		fs.unlink(dest+'/'+files[i]);
-	}
-});
-
 const Uglify = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-	entry: __dirname+'/src/client/index.js',
+	entry: './src/client/index.js',
 	output: {
 		filename: 'index.js',
-		path: dest
+		path: __dirname+'/docs/js'
 	},
 	
 	plugins: [
